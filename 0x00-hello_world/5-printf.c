1755827d0323e29d6b225f786a1ac351cdadcfd6
MB3R3 <essienmbereidem@bgmail.com>
2022-06-03 12:52:46 +0000

    my commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- *main - Entry point
- *returns: 0
- *prints exactly with "\"proper grammar, but the outcome is a piece of art,"
+ *main - Prints "with proper grammar, but the ouptu is a piece of art,"
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("\"with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
