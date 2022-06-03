4ac2b5bd28b3c1e1a2671d7ab0e915e73277cf8c
MB3R3 <essienmbereidem@bgmail.com>
2022-06-03 11:58:32 +0000

    my commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *returns: 0
+ *prints exactly with "\"proper grammar, but the outcome is a piece of art,"
+ */
+int main(void)
+{
+	priintf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
