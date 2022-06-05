32f7c05b2f643b397ce4d39eb82b689c066b526b
MB3R3 <essienmbereidem@bgmail.com>
2022-06-05 15:38:23 +0000

    my commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry point 		*
+ * main - Entry point
  * Return: 0
 */
-	int main(void)
-
-	{
+int main(void)
+{
 	int numR;
 
 	int numL;
@@ -14,21 +13,24 @@
 
 	for (numL = 48; numL <= 57; numL++)
 	{
-	for (numM = numL + 1; numM <= 57; numM++)
-	{
-	for (numR = numM + 1; numR <= 57; numR++)
-	{
-	if (numM != numR)
-																						{																					putchar(numL);
-																						putchar(numM);
-																						putchar(numR);
-																						if (!(
-																						numR == 57 && numM == 56 && numL == 55))
-																						{
-																						putchar(',');
-																						putchar(' ');
-																						}																					}
-	}
+		for (numM = numL + 1; numM <= 57; numM++)
+		{
+			for (numR = numM + 1; numR <= 57; numR++)
+			{
+				if (numM != numR)
+				{
+					putchar(numL);
+					putchar(numM);
+					putchar(numR);
+					if (!(
+								numR == 57 && numM == 56 && numL == 55))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
 	}
 	putchar('\n');
 	return (0);
