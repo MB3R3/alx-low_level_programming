7f22409d211924b79e07506d904733e8f84276d6
MB3R3 <essienmbereidem@bgmail.com>
2022-06-05 15:21:13 +0000

    my commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,19 +6,27 @@
  */
 int main(void)
 {
-	int digit1, digit2;
+	int numR;
 
-	for (digit1 = 0; digit1 < 9; digit1++)
+	int numL;
+
+	int minL = 48;
+
+	for (numL = 48; numL <= 57; numL++)
 	{
-	for (digit2 = digit1 + 1; digit2 < 10; digit2++)
-	}
-	putchar((digit1 % 10) + '0');
-	putchar((digit2 % 10) + '0');
-	if (digit1 == 8 && digit2 == 9)
-	continue;
-	putchar(',')
-	putchar(' ');
-	}
+		for (numR = minL++; numR <= 57; numR++)
+		{
+			if (numR != numL)
+			{
+				putchar(numL);
+				putchar(numR);
+				if (!(numR == 57 && numL == 56))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
 	}
 	putchar('\n');
 	return (0);
