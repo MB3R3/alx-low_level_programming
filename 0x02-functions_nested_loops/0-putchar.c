7cbf859c92666ace25ecf7b8b22576f3070625cb
MB3R3 <essienmbereidem@gmail.com>
2022-06-07 02:55:37 -0700

    my commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <putchar.h>
+/**
+ * Main - Entry point
+ * Description prints _putchar followed by a new line
+ * Return 0 - always
+ */
+
+int main(void)
+{
+	char putchar[] = "_putchar";
+
+	_putchar("%c\n", putchar);
+	return (0);
+}
+
