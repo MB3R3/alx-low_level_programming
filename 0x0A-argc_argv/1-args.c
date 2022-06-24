665a2abeb86b22e2218d1b8b4877557cdc9641b0
MB3R3 <essienmbereidem@gmail.com>
2022-06-24 11:37:19 +0000

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print number of argument passed into it
+ * @argc: argument counter
+ * @argv: pointer to the array of argument
+ * Return: 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	if (argc > 0)
+		printf("%d\n", argc - 1);
+
+	return (0);
+}
